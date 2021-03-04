f55463f3926aa604f017378e7bf7e3362ce9d96e
BenMabroukDhia <49234916+BenMabroukDhia@users.noreply.github.com>
2021-03-04 15:46:34 +0100

    Add files via upload

---
+++ rendez_vous.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 02, 2021 at 09:01 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bdpfe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rendez vous`
+--
+
+CREATE TABLE `rendez vous` (
+  `id_rv` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `lieu` varchar(20) COLLATE utf8_bin NOT NULL,
+  `raison` varchar(100) COLLATE utf8_bin NOT NULL,
+  `login` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rendez vous`
+--
+ALTER TABLE `rendez vous`
+  ADD PRIMARY KEY (`id_rv`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
